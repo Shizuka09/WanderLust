5f0c62d3f1cce936bd564ed8a694e3928aa740d1
Akriti Anand <156354405+Shizuka09@users.noreply.github.com>
2025-01-20 00:31:14 +0530

    review commit

--- models/listing.js
+++ models/listing.js
@@ -1,7 +1,9 @@
+  const { required } = require("joi");
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema ;
+  const Schema = mongoose.Schema ;
+  const Review = require("./review.js");
 
-const listingSchema = new Schema ({
+  const listingSchema = new Schema ({
     title :{
         type : String ,
         required : true ,
@@ -21,8 +23,20 @@ const listingSchema = new Schema ({
     price : Number ,
     location : String,
     country : String ,
-});
+    reviews : [{
+        type : Schema.Types.ObjectId,
+        ref : "Review",
+       },
+      ],
+    });
 
-const Listing = mongoose.model("Listing", listingSchema);
-module.exports = Listing ;
+    listingSchema.post("findOneAndDelete", async(listing) =>{
+     if(listing){
+          await Review.deleteMany({_id : {$in: listing.reviews}});
+     }
+
+    });
+
+    const Listing = mongoose.model("Listing", listingSchema);
+    module.exports = Listing ;
 

88c76eeb696f42bd1f3ab8b566a331e9239f97b2
Akriti Anand <156354405+Shizuka09@users.noreply.github.com>
2025-01-29 23:52:11 +0530

    login Styling Commit

--- routes/user.js
+++ routes/user.js
@@ -14,8 +14,13 @@
         const newUser = new User({email,username});
         const registeredUser = await User.register(newUser,password);
         console.log(registeredUser);
-        req.flash("success", "Welcome to Wanderlust!");
-        res.redirect("/listings");
+        req.login(registeredUser , (err) => {
+            if(err){
+                return next (err);
+            }
+            req.flash("success", "Welcome to Wanderlust!");
+            res.redirect("/listings");
+         });
        }
        catch(err){
           req.flash("error" , err.message); 
